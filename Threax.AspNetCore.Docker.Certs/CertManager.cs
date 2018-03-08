31518ae554d299a4edbd6e5fe488e69c73c9471c
Andrew Piper <andrew@threax.com>
2018-03-08 15:04:09 -0500

    Better comment and nuget info.

--- Threax.AspNetCore.Docker.Certs/CertManager.cs
+++ Threax.AspNetCore.Docker.Certs/CertManager.cs
@@ -2,15 +2,19 @@
 
 namespace Threax.AspNetCore.Docker.Certs
 {
+    /// <summary>
+    /// A class to manage certificates in the microsoft/aspnetcore:2.0 Linux docker image.
+    /// </summary>
     public static class CertManager
     {
         /// <summary>
-        /// Load trusted roots by calling update-ca-certificates. Good inside Linux containers
-        /// that support this function (like the microsoft/aspnetcore:2.0 image). Returns true
-        /// if the certs were loaded, false otherwise. Does not indicate how many certs were loaded
-        /// just the the commands completed successfully.
-        /// Right now this is just to centralize this call, this will
-        /// evolve as the docker integration evolves.
+        /// Load all certificates that can be found by update-ca-certificates. The best
+        /// thing to do is to mount any custom certificates into /usr/local/share/ca-certificates
+        /// and then call this function on startup. This will load the custom certificates dynamically
+        /// on container startup each time. This function will return true if the commands completed
+        /// sucessfully and false otherwise. This does not indicate if any certificates were loaded
+        /// or not. If run outside of a container this will likely always return false unless the
+        /// app can manage to run update-ca-certificates on your system.
         /// </summary>
         public static bool LoadTrustedRoots()
         {
